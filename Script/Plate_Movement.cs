bb725c61ac1b17e314ff92cb481c039abcd2d150
Niloofar Soleimany <soleimany.niloufar@gmail.com>
2024-12-03 14:12:26 +0330

    Add files via upload
    codes(camera/object_basic)

---
+++ Script/Plate_Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate_Movement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Vector3 movementDirection;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 1);
+        transform.Translate(movementDirection * Time.deltaTime * speed);
+        if (transform.position.x > 0.44f)
+        {
+            transform.position = new Vector3(0.44f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -0.44f)
+        {
+            transform.position = new Vector3(-0.44f, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Script/camera_movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public GameObject mainCharacter;
+    private Vector3 cameraPosition;
+    private Vector3 characterPosition;
+    private Vector3 distance;
+
+    void Start()
+    {
+        cameraPosition = this.transform.position;
+        characterPosition = mainCharacter.transform.position;
+        distance = characterPosition - cameraPosition;
+        //Debug.Log("Distance" + distance);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+    private void LateUpdate()
+    {
+        //Debug.Log("lateupdate");
+        characterPosition = mainCharacter.transform.position; //har lahze posiotion update she
+        cameraPosition = characterPosition - distance;
+        this.transform.position = cameraPosition;
+    }
+}
