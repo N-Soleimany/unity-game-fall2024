e3a58a4f9d9069ab3f7bcbde7970d114cf2fe12e
Niloofar Soleimany <soleimany.niloufar@gmail.com>
2025-01-02 20:09:42 +0330

    dynamicRoad+stackcollect+delete left items in sync with road loading

---
+++ Script/DynamicRoad.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicRoad : MonoBehaviour
+{
+    [SerializeField] GameObject[] obstacles; // Obstacles to avoid
+    [SerializeField] GameObject[] enemies;   // Enemies to fight or evade
+    [SerializeField] GameObject[] food;      // Food to collect for score
+    [SerializeField] Transform[] points;     // Spawn points for items
+
+    private List<GameObject> protectedObjects = new List<GameObject>(); // List of objects to protect
+
+    GameObject[] createdObstacles;
+    GameObject[] createdEnemies;
+    GameObject[] createdFood;
+
+    private void Start()
+    {
+        createdObstacles = new GameObject[points.Length];
+        createdEnemies = new GameObject[points.Length];
+        createdFood = new GameObject[points.Length];
+
+        RefreshItems();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("TheCamera"))
+        {
+            // Move the parent block forward when triggered
+            transform.parent.transform.position += new Vector3(0, 0, 15);
+
+            // Clear and refresh items
+            /*ClearItems(createdObstacles);
+            ClearItems(createdEnemies);
+            ClearItems(createdFood);*/
+
+            RefreshItems();
+            Debug.Log("!!!block moved!!!");
+        }
+    }
+
+    public void AddProtectedObject(GameObject obj)
+    {
+        if (!protectedObjects.Contains(obj))
+        {
+            protectedObjects.Add(obj);
+        }
+    }
+
+    public void RemoveProtectedObject(GameObject obj)
+    {
+        if (protectedObjects.Contains(obj))
+        {
+            protectedObjects.Remove(obj);
+        }
+    }
+
+    private void RefreshItems()
+    {
+        ClearItems(createdObstacles);
+        ClearItems(createdEnemies);
+        ClearItems(createdFood);
+
+        int arrayIndex = 0;
+
+        foreach (Transform point in points)
+        {
+            float randomValue = Random.value;
+
+            if (randomValue < 0.15f) // 15% chance for obstacle
+            {
+                createdObstacles[arrayIndex] = SpawnRandomItem(obstacles, point);
+            }
+            else if (randomValue < 0.30f) // 15% chance for enemy
+            {
+                createdEnemies[arrayIndex] = SpawnRandomItem(enemies, point);
+            }
+            else // 70% chance for food
+            {
+                createdFood[arrayIndex] = SpawnRandomItem(food, point);
+            }
+            arrayIndex++;
+        }
+    }
+
+    private void ClearItems(GameObject[] items)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] != null)
+            {
+                // Log the object details for debugging
+                Debug.Log($"Checking object: {items[i].name}");
+
+                if (protectedObjects.Contains(items[i]))
+                {
+                    Debug.Log($"Skipping protected object: {items[i].name}");
+                    continue;
+                }
+
+                if (items[i].transform.parent != null && items[i].transform.parent.CompareTag("Player"))
+                {
+                    Debug.Log($"Skipping object with Player as parent: {items[i].name}");
+                    continue;
+                }
+
+                Debug.Log($"Destroying object: {items[i].name}");
+                Destroy(items[i]);
+                items[i] = null; // Clear the reference
+            }
+        }
+    }
+
+
+    private GameObject SpawnRandomItem(GameObject[] itemArray, Transform spawnPoint)
+    {
+        if (itemArray.Length == 0) return null;
+
+        int randomIndex = Random.Range(0, itemArray.Length);
+        return Instantiate(itemArray[randomIndex], spawnPoint.position, Quaternion.identity);
+    }
+}
--- Script/Plate_Movement.cs
+++ Script/Plate_Movement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,23 +8,28 @@ public class Plate_Movement : MonoBehaviour
     [SerializeField] float speed;
     Vector3 movementDirection;
     bool alive = true;
-    uint score = 0;
+    int score = 0;
     [SerializeField] AudioClip[] characterClips;
     private AudioSource audiosource;
 
+    private List<GameObject> stack = new List<GameObject>(); // Stack for food objects
+    private DynamicRoad dynamicRoadScript; // Reference to DynamicRoad script
+
     void Start()
     {
-        audiosource = GetComponent<AudioSource>(); 
-        
+        audiosource = GetComponent<AudioSource>();
+
+        // Find the DynamicRoad script in the scene
+        dynamicRoadScript = FindObjectOfType<DynamicRoad>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-        if (alive) {
+        if (alive)
+        {
             movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 1);
             transform.Translate(movementDirection * Time.deltaTime * speed);
+
             if (transform.position.x > 0.44f)
             {
                 transform.position = new Vector3(0.44f, transform.position.y, transform.position.z);
@@ -32,23 +37,19 @@ public class Plate_Movement : MonoBehaviour
             if (transform.position.x < -0.44f)
             {
                 transform.position = new Vector3(-0.44f, transform.position.y, transform.position.z);
-                //test
             }
         }
-        
-        
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("obstacle"))
         {
             alive = false;
             Debug.Log("dead");
-            audiosource.clip = characterClips[1]; //changed the audioclip
-            audiosource.Play(); //play the audio
-
-
-        }     
+            audiosource.clip = characterClips[1];
+            audiosource.Play();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -56,10 +57,63 @@ public class Plate_Movement : MonoBehaviour
         if (other.transform.CompareTag("food"))
         {
             score++;
-            Debug.Log("score" + score);
-            audiosource.clip = characterClips[0]; 
-            audiosource.Play(); 
-        }
+            Debug.Log("score: " + score);
+            audiosource.clip = characterClips[0];
+            audiosource.Play();
 
+            // Add food object to the stack
+            stack.Add(other.gameObject);
+
+            // Notify DynamicRoad to protect this object
+            if (dynamicRoadScript != null)
+            {
+                dynamicRoadScript.AddProtectedObject(other.gameObject);
+            }
+
+            // Position the new food on top of the stack
+            Vector3 newPosition = transform.position + new Vector3(0, 0.1f * stack.Count, 0);
+            other.gameObject.transform.position = newPosition;
+            other.gameObject.transform.SetParent(transform); // Make the food a child of the plate
+        }
+        else if (other.transform.CompareTag("enemy"))
+        {
+            score--;
+            Debug.Log("score: " + score);
+            audiosource.clip = characterClips[1];
+            audiosource.Play();
+
+            // Remove the top food object from the stack if available
+            if (stack.Count > 0)
+            {
+                GameObject topFood = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+
+                // Notify DynamicRoad to unprotect this object
+                if (dynamicRoadScript != null)
+                {
+                    dynamicRoadScript.RemoveProtectedObject(topFood);
+                }
+
+                Destroy(topFood);
+
+                // Reposition the remaining objects in the stack
+                for (int i = 0; i < stack.Count; i++)
+                {
+                    if (stack[i] != null) // Ensure the object is not destroyed
+                    {
+                        Vector3 newPosition = transform.position + new Vector3(0, 0.05f * (i + 1), 0);
+                        stack[i].transform.position = newPosition;
+                    }
+                }
+            }
+
+            if (score < 0)
+            {
+                alive = false;
+                Debug.Log("dead");
+                audiosource.clip = characterClips[1];
+                audiosource.Play();
+            }
+        }
     }
 }
